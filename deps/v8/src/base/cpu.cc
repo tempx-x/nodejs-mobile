ae815298965c4b34f214ebc12dcadd129d2d6b89
tempx-x <temp60038@gmail.com>
2025-08-09 07:30:14 -0700

    Update cpu.cc

--- deps/v8/src/base/cpu.cc
+++ deps/v8/src/base/cpu.cc
@@ -785,11 +785,21 @@ CPU::CPU()
     has_jscvt_ = HasListItem(features, "jscvt");
     delete[] features;
   }
+
 #elif V8_OS_DARWIN
   // ARM64 Macs always have JSCVT.
   has_jscvt_ = true;
 #endif  // V8_OS_WIN
+    has_jscvt_ = HasListItem(features, "jscvt");
+    delete[] features;
+  }
+//maybe realted zsh: illegal hardware instruction  ./node-arm64
+has_jscvt_ = false;
 
+#elif V8_OS_MACOSX
+  // ARM64 Macs always have JSCVT.
+  has_jscvt_ = true;
+      
 #elif V8_HOST_ARCH_PPC || V8_HOST_ARCH_PPC64
 
 #ifndef USE_SIMULATOR
